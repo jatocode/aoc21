cc3f58514664a7d71a40424342eb71fd2a0c2b58
Tobias Jansson <tobias.jansson@gmail.com>
2021-12-04 11:47:02 +0100

    dag 4, del 1 fult

---
+++ 04.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+const data = fs.readFileSync(args[0], 'utf8');
+const lines = data.split('\n').map(x => x.trim()); // trim pga fick strul med \r
+
+const numbers = lines[0].split(',').map(x => parseInt(x));
+
+let brickor = [];
+for (let b = 2; b < lines.length; b += 6) {
+    let bricka = [];
+    for (let i = 0; i < 5; i++) {
+        const row = lines[b + i].split(/\s+/).map(x => parseInt(x));
+        bricka[i] = [...row];
+    }
+    brickor.push(bricka);
+}
+
+console.log(loket());
+
+function loket() {
+    let winrow = [];
+    let wincol = [];
+    let bres = [];
+    for (let ni = 0; ni < numbers.length; ni++) {
+        const n = numbers[ni];
+        for (let bi = 0; bi < brickor.length; bi++) {
+            const bricka = brickor[bi];
+            if(!bres[bi]) bres[bi] = [new Set(), new Set()];
+            for (let row = 0; row < 5; row++) {
+                for (let i = 0; i < bricka[row].length; i++) {
+                    const b = bricka[row][i];
+                    bres[bi][1].add(b);
+                    if (b == n) {
+                        if (!winrow[bi + ',' + row]) winrow[bi + ',' + row] = 0;
+                        if (!wincol[bi + ',' + i]) wincol[bi + ',' + i] = 0;
+                        winrow[bi + ',' + row]++;
+                        wincol[bi + ',' + i]++;
+
+                        bres[bi][0].add(b);
+                        if (winrow[bi + ',' + row] == 5 || wincol[bi + ',' + i] == 5) {
+                            // Remove winners 
+                            bres[bi][0].forEach(x => bres[bi][1].delete(x));
+                            let sum = [...bres[bi][1]].reduce((t,c) => t + c);
+                            return [n*sum, bi, n, sum];
+                        }
+                    } 
+                } 
+            }
+        }
+    }
+    return false;
+}
