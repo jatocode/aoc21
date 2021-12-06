73e483cd86885044238e509812eb1f3b42337372
Tobias Jansson <Tobias.Jansson@prevas.se>
2021-12-06 15:39:58 +0100

    Mätte tid

--- 06.js
+++ 06.js
@@ -7,6 +7,9 @@ const fish = input.split(',').map(x => parseInt(x));
 let school = [...fish];
 let days = 80;
 
+let start = Date.now();
+
+// Dek 1, simulerar varje fisk för sig
 for (let day = 1; day <= days; day++) {
     let newfish = 0;
     let newschool = [];
@@ -24,24 +27,26 @@ for (let day = 1; day <= days; day++) {
     school = [...newschool];
 }
 
-console.log('Del 1 efter', days, 'dagar, totalt:', school.length);
+console.log('Del 1 efter', days, 'dagar, totalt:', school.length, ', time to calc:',Date.now() - start, 'ms');
 
 // Del 2, vi gör om...fick tänka en hel del
-// Räknar hur många som är i varje timer/cykel istället för totalt
+
+start = Date.now();
+// Räknar hur många som är i varje timer/cykel istället för att hålla koll på fiskarna
 let cycle = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 fish.forEach(age => cycle[age] += 1);
 
 days = 256;
 for (let day = 0; day < days; day++) {
     // Plocka ut de som ska göra nya fiskar, rotera allt ett steg
-    const [newFish, ...fishCycle] = cycle;
+    const [created, ...restCycle] = cycle;
     
     // Lägg de sist
-    cycle = [...fishCycle, newFish];
+    cycle = [...restCycle, created];
 
     // Öka antalet som har timer 6 med lika många som ska bli nya
-    cycle[6] += newFish;
+    cycle[6] += created;
 }
 
 let fiskar = cycle.reduce((a, fish) => a + fish);
-console.log('Del 2 efter', days, 'dagar, totalt:', fiskar);
+console.log('Del 2 efter', days, 'dagar, totalt:', fiskar, ', time to calc:',Date.now() - start, 'ms');
