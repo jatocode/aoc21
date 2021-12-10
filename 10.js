d1b2fa86de0059db9e49fce10df4e8a75631bd33
Tobias Jansson <Tobias.Jansson@prevas.se>
2021-12-10 16:04:51 +0100

    10.2 ostädad

--- 10.js
+++ 10.js
@@ -7,11 +7,14 @@ const left = "([{<";
 const right = ")]}>";
 
 let errors = [];
+let incomplete = [];
 
 //findCorrupt(lines[2]);
 
 lines.forEach(line => {
-    errors.push(findCorrupt(line));
+    let [err, incl] = findCorrupt(line);
+    errors.push(err);
+    incomplete.push(incl);
 });
 
 let score = 0;
@@ -20,16 +23,32 @@ errors.forEach(e => {
     if (e.length > 0) {
         let scores = [3, 57, 1197, 25137];
         let c = e[0];
-        //console.log(e[0], scores[right.indexOf(e[0])]);
         score += scores[right.indexOf(c)];
     }
 })
 
 console.log('Del 1:', score);
 
+let compscores = [];
+incomplete.forEach(ic => {
+    score = 0;
+    if (ic.length > 0) {
+        ic[0].forEach(c => {
+            score *= 5;
+            let scores = [1, 2, 3, 4];
+            score += scores[right.indexOf(c)];
+        });
+        compscores.push(score);
+    }
+})
+console.log(compscores.sort((a,b) => a-b).join());
+let median = compscores[Math.floor(compscores.length/2)];
+console.log('Del 2:', median);
+
 function findCorrupt(line) {
     let errors = [];
     let stack = [];
+    let incomplete = [];
 
     for (let i = 0; i < line.length; i++) {
         const c = line[i];
@@ -38,27 +57,27 @@ function findCorrupt(line) {
             // vi har en öppning, lägg matchande på stacken
             let closer = right[left.indexOf(c)];
             stack.push(closer);
-            console.log(stack.join(' '));
+            //  console.log(stack.join(' '));
         }
         if (matchRight(c) > 0) {
             let last = stack[stack.length - 1];
             if (c == last) {
                 stack.pop();
-                console.log(stack.join(' '));
+                //      console.log(stack.join(' '));
             } else {
-                console.log('found',c,'expected',last);
+                //      console.log('found',c,'expected',last);
                 errors.push(c);
                 break;
             }
         }
+
+        if (i == line.length - 1) {
+            console.log('unclosed at end', stack.join(''), 'to close', stack.reverse().join(''));
+            incomplete.push(stack);
+        }
     }
 
-    console.log('unclosed', stack.join(' '));
-
-    // if (errors.length > 0) {
-    //     console.log('--', errors);
-    // }
-    return errors;
+    return [errors, incomplete];
 }
 
 function matchRight(c) {
