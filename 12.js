caedf241425e82c27d595f7515c1bf65fe092828
Tobias Jansson <tobias.jansson@gmail.com>
2021-12-12 23:45:02 +0100

    Städade 12.2, skäms inte längre

--- 12.js
+++ 12.js
@@ -8,32 +8,32 @@ let lines = input.split(/\r?\n/);
 let caves = [];
 let small = [];
 lines.forEach(line => {
-    const [_, a,b] = line.match(/([a-zA-Z]+)-([a-zA-Z]+)/);
-    if(caves[a] == undefined) caves[a] = [];
-    if(caves[b] == undefined) caves[b] = [];
+    const [_, a, b] = line.match(/([a-zA-Z]+)-([a-zA-Z]+)/);
+    if (caves[a] == undefined) caves[a] = [];
+    if (caves[b] == undefined) caves[b] = [];
     caves[b].push(a);
     caves[a].push(b);
 
-    if(a == a.toLowerCase()) small[a] = 0;
-    if(b == b.toLowerCase()) small[b] = 0;
+    if (a == a.toLowerCase()) small[a] = 0;
+    if (b == b.toLowerCase()) small[b] = 0;
 });
 
 let paths = [];
-dfs('start',[],paths, (c,p) => part1Rule(c,p));
+dfs('start', [], paths, (c, p) => part1Rule(c, p));
 console.log('Del 1, antal vägar', paths.length);
 
 paths = [];
-dfs('start',[],paths, (c,p) => part2Rule(c,p));
+dfs('start', [], paths, (c, p) => part2Rule(c, p));
 //console.table(paths);
 console.log('Del 2, antal vägar', paths.length);
 
 function dfs(currcave, path, paths, rule) {
 
-    if(rule(currcave, path)) {
+    if (rule(currcave, path)) {
         return;
     }
 
-    if(currcave == 'end') {
+    if (currcave == 'end') {
         let np = [...path];
         np.push(currcave);
         paths.push(np);
@@ -41,7 +41,7 @@ function dfs(currcave, path, paths, rule) {
     }
     let connections = caves[currcave];
 
-    connections.forEach(c => {    
+    connections.forEach(c => {
         let np = [...path];
         np.push(currcave);
         dfs(c, np, paths, rule);
@@ -54,23 +54,13 @@ function part1Rule(cave, path) {
 }
 
 function part2Rule(cave, path) {
-    if(cave == cave.toLowerCase()) {
-        if(cave == 'start' && path.includes(cave)) return true;
-        let smvisits = path.filter(c => c == c.toLowerCase() && c != 'start' && c != 'end');
-        //console.log(path,cave, smvisits);
-        let small = [];
+    if (cave == 'start' && path.includes(cave)) return true;
 
-        // [ 'start', 'b', 'A', 'b', 'A' ] c [ 'b', 'b' ]
-        // [ b: 2 ] true
+    // Antal små
+    let smvisits = path.filter(c => c == c.toLowerCase());
+    // Antal unika små
+    let uq = [...new Set(smvisits)];
 
-        let once = 0;
-        smvisits.forEach(c => {
-            if(!small[c]) small[c] = 0;
-            small[c]++;
-            if(small[c] > 1) once++;
-        });
-        //console.log(small, once);
-        return once > 1;
-    } 
-    return false;
+    // Är antalet 2 mer än unika så har vi försökt gå in i två smågrottor två gånger
+    return (smvisits.length - uq.length) > 1
 }
