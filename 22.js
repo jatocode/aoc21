7cfec6ff0eaac40629833250b50e57f84eb1c584
Tobias Jansson <Tobias.Jansson@prevas.se>
2021-12-22 09:11:54 +0100

    22.2

--- 22.js
+++ 22.js
@@ -4,48 +4,48 @@ const args = process.argv.slice(2);
 const input = fs.readFileSync(args[0], 'utf8');
 let lines = input.split(/\r?\n/);
 
-let algo;
-let imgMap = new Map();
-let [width, height] = [0, 0];
+let core = new Set();
 for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
 
-    const [] = line.match(/(on|off) x=(\d+)..)
+    if(line.length == 0) continue;
+
+    // on x=10..12,y=10..12,z=10..12
+    const [_, state, coords] = line.match(/(on|off) (.*)/);
+    let [__, xp, xa, yp, ya, zp, za] = coords.match(/x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/).map(x => parseInt(x));
+
+    let size = core.size;
+    [xp, xa] = [min50(xp), max50(xa)];
+    [yp, ya] = [min50(yp), max50(ya)];
+    [zp, za] = [min50(zp), max50(za)];
+
+    for (let z = zp; z <= za; z++) {
+        for (let y = yp; y <= ya; y++) {
+            for (let x = xp; x <= xa; x++) {
+                const cube = x + ',' + y + ',' + z;
+                if (in50(x) && in50(y) && in50(z)) {
+                    if (state == 'on') {
+                        core.add(cube);
+                    } else {
+                        core.delete(cube);
+                    }
+                }
+            }
+        }
+    }
+    // console.log(state, xp, xa, xp, ya, zp, za, '->', core.size - size);
 };
 
+console.log('Core size', core.size);
 
-function enhance(x, y) {
-    let pix = neighboursPix(x, y);
-    let np = ninepixel(pix.join(''));
-
-    return np;
+function in50(a) {
+    return a >= -50 && a <= 50;
 }
 
-function ninepixel(pixels) {
-    return algo[parseInt(pixels, 2)]
+function max50(a) {
+    return a >= 50 ? 50 : a;
 }
 
-function pixel(x, y) {
-    let p = imgMap.get(x + ':' + y);
-    
-    // Visar sig att "infinity" blinkar varje steg pga algoritmen. TRICKY!
-    let bg = step % 2 == 0 ? '1' : '0';
-    
-    return p == undefined ? bg : p;
+function min50(a) {
+    return a <= -50 ? -50 : a;
 }
-
-function neighboursPix(x, y) {
-    return [
-        pixel((x - 1), (y - 1)), // NV
-        pixel(x, (y - 1)),   // N
-        pixel((x + 1), (y - 1)), // NO
-
-        pixel((x - 1), y),         // V
-        pixel(x, y),  // MITT
-        pixel((x + 1), y),     // O
-
-        pixel((x - 1), (y + 1)), // SV
-        pixel(x, (y + 1)),       // S
-        pixel((x + 1), (y + 1)), // SO
-    ];
-};
